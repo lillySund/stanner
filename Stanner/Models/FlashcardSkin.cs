0e5aafe83ca5a9cd91d48f14e96f7e0193ecdd63
Lilly Sundholm <lillysu@duck.com>
2026-02-21 22:46:02 +0000

    Amend

--- Stanner/Models/FlashcardSkin.cs
+++ Stanner/Models/FlashcardSkin.cs
@@ -6,7 +6,7 @@ public class FlashcardSkin
     public required string Name { get; set; }
     public string? Description { get; set; }
     public int XPCost { get; set; }
-    public required string CssClass { get; set; } // css for styling
+    public required string CssClass { get; set; } // css for styling :3
     public string? PreviewImage { get; set; }
     public bool IsDefault { get; set; } = false;
 }   
